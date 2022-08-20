9c772aa8ebecb4e6dd6cfe69f48c078f81462c68
Shawon Ibn Kamal <shawon.1165@gmail.com>
2022-08-20 13:03:53 -0230

    Driver api

--- index.js
+++ index.js
@@ -39,13 +39,15 @@ const searchDrivers = async (phone, address) => {
                     if (err) throw err;
                 });
 
-                
+
 
             }
         });
         await sleep(2000);
     }
 }
+
+searchDrivers("+17091234567", "299 Elizabeth Ave");
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -63,6 +65,45 @@ app.post('/sms', urlencodedParser, (req, res) => {
 
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
+});
+
+// driver get info
+app.get('/driver', (req, res) => {
+    const phone = req.query.phone;
+    if (!phone) {
+        res.send({message: "Phone number required!"});
+        return;
+    }
+    console.log(phone);
+    con.query(`SELECT * FROM drivers WHERE phone = "${phone}"`, function (err, results, fields) {
+        if (err) throw err;
+        if (results.length > 0) {
+            let driver = results[0];
+            const response = {
+                isdriving: driver.isdriving ? true : false,
+                address: driver.isdriving ? driver.isdriving : "",
+                phone: driver.isdriving ? driver.phone : "",
+            }
+            res.send(response);
+            return;
+        } else {
+            res.send({message: "No driver found"});
+            return;
+        }
+    });
+});
+
+// stop driving
+app.post('/stopdriving', (req, res) => {
+    const phone = req.query.phone;
+    if (!phone) {
+        res.send({message: "Phone number required!"});
+        return;
+    }
+    con.query(`UPDATE drivers SET isdriving = NULL WHERE phone = "${phone}"`, function (err, results, fields) {
+        if (err) throw err;
+        res.send({message: "Stopped driving"});
+    });
 });
 
 app.listen(port, () => {
